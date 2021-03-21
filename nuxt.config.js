62e066e797d492e78cb33165413b0d54ac437930
Rafael Sales <rafael.sales93@hotmail.com>
2021-03-20 21:16:27 -0300

    Update default port server.

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,6 +16,10 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: ['@/assets/css/main.css'],
 
+  server: {
+    port: 5000,
+  },
+
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
 
