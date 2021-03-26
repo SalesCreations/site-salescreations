f7bd9e5c7b46754d59e93436cf7f8bc294bf8996
Rafael Sales <rafael.sales93@hotmail.com>
2021-03-26 08:06:09 -0300

    Fix layout banner cta.

--- components/banners/BannerCta.vue
+++ components/banners/BannerCta.vue
@@ -1,6 +1,6 @@
 <template>
-  <div id="banner-cta" class="p-10 rounded border border-black flex items-center">
-    <h3 class="banner-title text-4xl font-bold leading-none flex-grow">Download my resume</h3>
+  <div id="banner-cta" class="p-10 rounded border border-black md:flex items-center">
+    <h3 class="banner-title text-4xl font-bold leading-none flex-grow mb-5 md:mb-auto">Download my resume</h3>
     <ButtonMore label="Download my resume" to="/" />
   </div>
 </template>
