a0e4b944f47236f5de6f30b2e5155c90a50721a7
Rafael Sales <rafael.sales93@hotmail.com>
2021-05-11 21:05:46 -0300

    Create structure component CardProject.

--- components/cards/CardProject.vue
+++ components/cards/CardProject.vue
@@ -1,6 +1,15 @@
 <template>
-  <div id="card-project" class="py-16 px-10 bg-gray-200 my-5 rounded">
-    <NuxtLink to="/" class="link-post">
+  <div id="card-project" class="py-8 px-10 bg-white my-5 rounded border-2 border-black">
+    <NuxtLink to="/" class="link-post flex">
+      <div class="descriptions">
+        <p class="tag-project text-sm font-thin">Project - 2019</p>
+        <h3 class="title-project text-3xl font-black">Lorem ipsum dolor sit amet</h3>
+        <p class="intro-project text-base font-thin">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+          aliqua.
+        </p>
+      </div>
+      <img src="@/assets/images/work-demo.png" alt="image-porject" width="192" height="113" />
     </NuxtLink>
   </div>
 </template>
