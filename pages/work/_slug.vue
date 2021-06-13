84723668dfabf0ae1183d7db4a3b7539136ddb4b
Rafael Sales <rafael.sales93@hotmail.com>
2021-06-12 23:10:02 -0300

    Include tec info show project.

--- pages/work/_slug.vue
+++ pages/work/_slug.vue
@@ -3,8 +3,41 @@
     <header class="header grid gap-4 grid-cols-10 py-10">
       <div class="col-span-10 flex flex-col flex-wrap content-center">
         <h1 class="text-4xl md:text-5xl font-black mb-2">{{ project.title }}</h1>
-        <!-- <div class="image-project" :style="`background-image: url('${project.cover.url}')`" /> -->
         <img class="image-project" :src="project.cover.url" :alt="project.cover.title" width="895.875" height="318.719" />
+        <div class="infos-project my-10 py-6 border-t border-b grid grid-flow-row grid-cols-5 grid-rows-1 gap-4">
+          <div class="company-info">
+            <h6>Company</h6>
+            <p>{{ project.company }}</p>
+          </div>
+          <div class="team-info">
+            <h6>Team</h6>
+            <ul>
+              <li v-for="(people, key) in project.team" :key="`people--${key}`">
+                {{ people }}
+              </li>
+            </ul>
+          </div>
+          <div class="tools-info">
+            <h6>Tools</h6>
+            <ul>
+              <li v-for="(tool, key) in project.tools" :key="`tool--${key}`">
+                {{ tool }}
+              </li>
+            </ul>
+          </div>
+          <div class="roles-info">
+            <h6>Roles</h6>
+            <ul>
+              <li v-for="(role, key) in project.roles" :key="`role--${key}`">
+                {{ role }}
+              </li>
+            </ul>
+          </div>
+          <div class="year-info">
+            <h6>Year</h6>
+            <p>{{ project.year }}</p>
+          </div>
+        </div>
       </div>
     </header>
   </div>
