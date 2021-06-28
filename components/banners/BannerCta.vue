2df7e4d43271b3ca7e21b63818da236942adcca5
Rafael Sales <rafael.sales93@hotmail.com>
2021-06-27 23:54:32 -0300

    Include Curriculum vitae and icon linkedin.

--- components/banners/BannerCta.vue
+++ components/banners/BannerCta.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="banner-cta" class="p-10 rounded border border-black md:flex items-center">
     <h3 class="banner-title text-4xl font-bold leading-none flex-grow mb-5 md:mb-auto">Download my resume</h3>
-    <ButtonMore label="Download my resume" to="/" />
+    <a href="/curriculum-vitae-rafael.pdf" class="link flex button-more">
+      Download my resume
+      <svg class="ArrowRightAlt" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M16.01 11H4V13H16.01V16L20 12L16.01 8V11Z" fill="#000000" />
+      </svg>
+    </a>
   </div>
 </template>
 
@@ -12,3 +17,9 @@ export default Vue.extend({
   name: 'BannerCta',
 })
 </script>
+
+<style scoped>
+.button-more {
+  max-width: max-content;
+}
+</style>
