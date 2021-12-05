53b7f7fe4da13d7bd816c7df3853844e702190b6
rsales <rafael.sales93@hotmail.com>
2021-12-05 14:21:07 -0300

    Bug get token - 3.

--- services/common/http-storyblok.js
+++ services/common/http-storyblok.js
@@ -1,14 +1,10 @@
 import axios from 'axios'
 
 export const apiClient = axios.create({
-  baseURL: `https://api.storyblok.com/v2/cdn/stories/`,
+  baseURL: `https://api.storyblok.com/v2/cdn/stories/?resolve_links=1&token=${process.env.ACCESS_TOKEN_SB}`,
   whithCredentials: false,
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
-  },
-  params: {
-    resolve_links: 1,
-    token: process.env.ACCESS_TOKEN_SB,
   },
 })
