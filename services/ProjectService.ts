383fa478990b0df459b308e9d874f6406ac3587a
rsales <rafael.sales93@hotmail.com>
2021-12-05 14:43:28 -0300

    Bug get token - 6.

--- services/ProjectService.ts
+++ services/ProjectService.ts
@@ -2,7 +2,7 @@ import { apiClient } from './common/http-storyblok'
 
 export default {
   getProjects(payload: any) {
-    return apiClient.get(`/?token=${process.env.ACCESS_TOKEN_SB}`, {
+    return apiClient.get(`/?token=${process.env.tokenStoryblok}`, {
       params: {
         starts_with: payload.path.substr(1),
         version: payload.query._storyblok !== undefined || payload.isDev ? 'draft' : 'published',
@@ -10,7 +10,7 @@ export default {
     })
   },
   getProjectsRoutes(payload: any) {
-    return apiClient.get(`/?token=${process.env.tokenStoryblok}`, {
+    return apiClient.get(`/?token=${process.env.ACCESS_TOKEN_SB}`, {
       params: {
         starts_with: payload.path.substr(1),
         version: payload.isDev ? 'draft' : 'published',
