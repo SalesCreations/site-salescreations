da458fe0d50ae621d634157092f8c23c586939d2
rsales <rafael.sales93@hotmail.com>
2021-12-05 14:32:26 -0300

    Bug get token - 4.

--- services/PostService.ts
+++ services/PostService.ts
@@ -2,7 +2,7 @@ import { apiClient } from './common/http-storyblok'
 
 export default {
   getPosts(payload: any) {
-    return apiClient.get('', {
+    return apiClient.get(`/?token=${process.env.ACCESS_TOKEN_SB}`, {
       params: {
         starts_with: payload.path.substr(1),
         version: payload.query._storyblok !== undefined || payload.isDev ? 'draft' : 'published',
@@ -10,7 +10,7 @@ export default {
     })
   },
   getPostsRoutes(payload: any) {
-    return apiClient.get('', {
+    return apiClient.get(`/?token=${process.env.TokenStoryblok}`, {
       params: {
         starts_with: payload.path.substr(1),
         version: payload.isDev ? 'draft' : 'published',
--- services/common/http-storyblok.js
+++ services/common/http-storyblok.js
@@ -1,10 +1,14 @@
 import axios from 'axios'
 
 export const apiClient = axios.create({
-  baseURL: `https://api.storyblok.com/v2/cdn/stories/?resolve_links=1&token=${process.env.ACCESS_TOKEN_SB}`,
+  baseURL: `https://api.storyblok.com/v2/cdn/stories/`,
   whithCredentials: false,
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
+  },
+  params: {
+    resolve_links: 1,
+    token: process.env.ACCESS_TOKEN_SB,
   },
 })
