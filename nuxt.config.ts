12c9b6ea5f56a1eb6563669293e6f3596f95356e
rsales <rafael.sales93@hotmail.com>
2022-06-09 00:23:59 -0300

    Remove title page head nuxt config.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,16 +8,10 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Sales//Creations co-creation solutions',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'og:image',
-        property: 'og:image',
-        content: `${'https://salescreations.com' + require('@/assets/images/thumbnail-site.png')}`,
-      },
       {
         hid: 'theme-color',
         name: 'theme-color',
