33135b2bbfc06a264b7ca8ca7edd2339f42ad493
rsales <rafael.sales93@hotmail.com>
2023-01-23 22:14:11 -0300

    Correction import env in plugin analytic.ts

--- plugins/analytic.client.ts
+++ plugins/analytic.client.ts
@@ -1,13 +1,15 @@
 import Analytics from 'analytics'
 import segmentPlugin from '@analytics/segment'
 
-const analytics = Analytics({
-  app: 'site-salescreations',
-  plugins: [
-    segmentPlugin({
-      writeKey: `${process.env.WRITE_CODE_SEGMENT}`
-    })
-  ]
-})
+export default defineNuxtPlugin((nuxtApp) => {
+  const runtimeConfig = useRuntimeConfig();
 
-export default defineNuxtPlugin((nuxtApp) => {})
+  const analytics = Analytics({
+    app: 'site-salescreations',
+    plugins: [
+      segmentPlugin({
+        writeKey: runtimeConfig.public.writeCodeSegment
+      })
+    ]
+  })
+})
