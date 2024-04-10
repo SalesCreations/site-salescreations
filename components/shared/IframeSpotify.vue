ae5c23be80a6eefaecc93d92a5f1f4b47101a7db
rsales <rafael.sales93@hotmail.com>
2024-04-10 10:27:32 -0300

    Update IframeSpotify component.

--- components/shared/IframeSpotify.vue
+++ components/shared/IframeSpotify.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-    <iframe style="border-radius:12px" :src="blok.url.url" width="100%" height="200" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+    <iframe
+      style="border-radius:12px"
+      :src="blok.url.url" 
+      width="100%" 
+      height="200"
+      class="w-full aspect-auto"
+      frameBorder="0" 
+      allowfullscreen="" 
+      allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+      loading="lazy"
+    ></iframe>
   </div>
 </template>
 
